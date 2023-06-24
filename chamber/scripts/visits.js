feb8dd2920bcd4455f9b18ec103e7f23b767ac2c
chesnaanderson <c.pearl.anderson@gmail.com>
2023-06-24 17:22:22 -0500

    new file:   chamber/discover.html 	new file:   chamber/images/Mueller_Park_Trail_in_Bountiful.webp 	new file:   chamber/images/bountiful_five_points_park.webp 	new file:   chamber/images/bountiful_main_st_2.webp 	new file:   chamber/images/bountiful_main_st_3.webp 	new file:   chamber/images/bountiful_main_st_4.webp 	new file:   chamber/images/bountiful_main_st_small.webp 	modified:   chamber/index.html 	new file:   chamber/scripts/visits.js 	new file:   chamber/styles/base copy.css 	modified:   chamber/styles/base.css

---
+++ chamber/scripts/visits.js
@@ -0,0 +1,54 @@
+var counterContainer = document.querySelector(".website-counter");
+var result = document.querySelector("result");
+var visitCount = localStorage.getItem("page_view");
+// milliseconds to days constant = 1000 ms/s * 60 s/m * 60 m/h * 24 h/day
+const msToDays = 84600000;
+// today's date
+const theDateToday = new Date();
+const todayElement = document.querySelector("#today");
+const today = Date.now();
+// Check if page_view entry is present
+// function getNumberOfDays(start, end) {
+//     const date1 = new Date(start);
+//     const date2 = new Date(end);
+
+//     // One day in milliseconds
+//     const oneDay = 1000 * 60 * 60 * 24;
+
+//     // Calculating the time difference between two dates
+//     const diffInTime = date2.getTime() - date1.getTime();
+
+//     // Calculating the no. of days between two dates
+//     const diffInDays = Math.round(diffInTime / oneDay);
+
+//     return diffInDays;
+// }
+
+// console.log(getNumberOfDays("page_view", "today"));
+//   function lastVisited(){
+//     let text = document.lastModified;
+//     document.getElementById("lastVisited").innerHTML = text;
+//   }
+  
+// lastVisited();
+if (today) {
+  visitCount != Number(today) + 1;
+  localStorage.setItem("page_view", "Welcome! Let us know if you have any questions.");
+} 
+if(visitCount, today) {
+  visitCount < Number(today);
+  localStorage.setItem("page_view", "you last visited", "lastVisited");
+}
+if(visitCount) {
+    visitCount == Number(today) * 2;
+    localStorage.setItem("page_view", "Back so soon? Awesome!");
+  }
+else{
+    visitCount = 1;
+    localStorage.setItem("page_view", "what's up?");
+}
+counterContainer.innerHTML = visitCount;
+
+
+
+
