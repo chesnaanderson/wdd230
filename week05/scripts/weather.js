938a5bc3456d7e99e63ae69d0cb2236535a2c20a
chesnaanderson <c.pearl.anderson@gmail.com>
2023-07-05 22:49:10 -0500

    modified:   index.html 	new file:   week05/scripts/weather.js 	new file:   week05/weather.html

---
+++ week05/scripts/weather.js
@@ -0,0 +1,32 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&appid=093427f70323ff470e315f773e308498';
+
+async function apiFetch(url) {
+    try{
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data)
+            //console.log()
+        } else {
+            throw Error(await response.text());
+        } 
+    } catch (error) {
+        console.log("API request failed");
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;f`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
+
+apiFetch(url);
