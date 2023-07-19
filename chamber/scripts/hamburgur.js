3fa7258b6b422a39082d08f677dab74801c45661
chesnaanderson <c.pearl.anderson@gmail.com>
2023-07-18 23:36:57 -0500

    modified:   chamber/directory.html 	modified:   chamber/discover.html 	modified:   chamber/index.html 	modified:   chamber/scripts/hamburgur.js 	modified:   chamber/styles/base.css 	modified:   chamber/styles/weather.css 	modified:   chamber/thankyou.html

--- chamber/scripts/hamburgur.js
+++ chamber/scripts/hamburgur.js
@@ -5,3 +5,12 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+function myFunction() {
+	var x = document.getElementById("myLinks");
+	if (x.style.display === "block") {
+	  x.style.display = "none";
+	} else {
+	  x.style.display = "block";
+	}
+  }
